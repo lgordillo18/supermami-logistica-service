5a9dd4893237a7a702d3050b27dc48a9edfe0d6c
Lucas Gordillo <lucasdgordillo@gmail.com>
2021-10-17 18:16:45 -0300

    Fixing Roles endpoint

--- src/main/java/com/supermamilogisticaservice/services/UserService.java
+++ src/main/java/com/supermamilogisticaservice/services/UserService.java
@@ -2,6 +2,7 @@ package com.supermamilogisticaservice.services;
 
 import com.supermamilogisticaservice.models.Rol;
 import com.supermamilogisticaservice.models.User;
+import com.supermamilogisticaservice.repositories.IRolRepository;
 import com.supermamilogisticaservice.repositories.IUserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class UserService {
   @Autowired
   private IUserRepository iUserRepository;
+  private IRolRepository iRolRepository;
 
   public User saveUser(User user) {
     return iUserRepository.save(user);
@@ -21,9 +23,9 @@ public class UserService {
   public ArrayList<User> getAllUsers(){
     return (ArrayList<User>) iUserRepository.findAll();
   }
-  
-  public ArrayList<Rol> getRoles(Integer id){
-    return iUserRepository.findByRoles(id);
+
+  public ArrayList<Rol> getAllRoles(){
+    return (ArrayList<Rol>) iRolRepository.findAll();
   }
   
   public Optional<User> getUser (Integer id){
