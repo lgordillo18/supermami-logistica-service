89a17744ae4ad2a89c9d3952c30e079a1ce1c71a
Your Name <you@example.com>
2021-10-24 00:53:59 -0300

    API Test Actualizar Usuario

--- src/test/java/com/supermamilogisticaservice/UserTests.java
+++ src/test/java/com/supermamilogisticaservice/UserTests.java
@@ -1,5 +1,6 @@
 package com.supermamilogisticaservice;
 
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +10,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class UserTests {
@@ -19,5 +21,61 @@ public class UserTests {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/api/logistica-service/users")).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         assertThat(response.statusCode()).isEqualTo(200);
+    }
+
+    @Test
+    void ensureUpdateUserAPIRequestWorks() throws Exception{
+
+        JSONObject jObject = new JSONObject();
+        jObject.put("first_name", "Test");
+        jObject.put("last_name", "Testerino");
+        jObject.put("password", "drowssap");
+        jObject.put("username", "Testy");
+        jObject.put("dni", "44444444");
+        jObject.put("id_area", new Integer(1));
+
+        String jsonBodyPOST = jObject.toJSONString();
+
+        HttpClient client = HttpClient.newBuilder().build();
+
+        HttpRequest requestPOST = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/api/logistica-service/user"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBodyPOST))
+                .build();
+        HttpResponse<String> responsePOST = client.send(requestPOST, HttpResponse.BodyHandlers.ofString());
+        String bodyResponsePOST = responsePOST.body();
+        System.out.println(bodyResponsePOST);
+
+
+        JSONObject jsonObjectPUT = new JSONObject();
+
+
+        jsonObjectPUT.put("id", new Integer(1));
+        jsonObjectPUT.put("first_name", "David");
+        jsonObjectPUT.put("last_name", "Testerino");
+        jsonObjectPUT.put("password", "drowssap");
+        jsonObjectPUT.put("phone_number", null);
+        jsonObjectPUT.put("email", null);
+        jsonObjectPUT.put("address", null);
+        jsonObjectPUT.put("office", null);
+        jsonObjectPUT.put("area", null);
+        jsonObjectPUT.put("rol", null);
+        jsonObjectPUT.put("active", new Boolean((true)));
+        jsonObjectPUT.put("username", "Testy");
+        jsonObjectPUT.put("dni", "44444444");
+
+        String jsonBodyPUT = jsonObjectPUT.toJSONString();
+
+        HttpRequest requestPUT = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/api/logistica-service/user/1"))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(jsonBodyPUT))
+                .build();
+        HttpResponse<String> responsePUT = client.send(requestPUT, HttpResponse.BodyHandlers.ofString());
+        String bodyResponsePUT = responsePUT.body();
+        System.out.println(bodyResponsePUT);
+        assertTrue(bodyResponsePUT.contains("first_name\":\"David\""));
+
     }
 }
