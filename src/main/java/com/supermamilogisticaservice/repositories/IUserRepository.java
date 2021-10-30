f9ea3d8139a7e82fa6af1b93f2840dd11f875c28
Angeles-Cerdosino <angelescerdosi@gmail.com>
2021-10-30 00:40:37 -0300

    borro funcion que no se usa

--- src/main/java/com/supermamilogisticaservice/repositories/IUserRepository.java
+++ src/main/java/com/supermamilogisticaservice/repositories/IUserRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
   List<User> findByRol(Optional<Rol> rol);
-  public abstract ArrayList<Rol> findByRoles(Integer id);
+
 }
